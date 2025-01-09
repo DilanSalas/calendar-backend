6f231328f59473712641a3f07724ed5eb8d0e322
Dilan <dilansalas200@gmail.com>
2025-01-08 23:47:16 -0600

    update events

--- controllers/events.js
+++ controllers/events.js
@@ -47,12 +47,42 @@ const crearEvento = async(req,res = response)=>{
 
 const actualizarEvento = async(req,res = response)=>{
 
-    res.status(201).json(
-        {
-            ok:true,
-            msg: 'evento actualizadp'
-           
-        })
+    const eventoId = req.params.id
+
+    try {
+
+        const evento = await Evento.findById(eventoId);
+
+        if(!evento){
+            res.status(404).json({ok: false, msg: 'El evento no existe'});
+            return;
+        }
+
+        if(evento.user._id.toString() !== req.uid){
+            res.status(401).json({ok: false, msg: 'No tienes permisos para editar este evento'});
+            return;
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: req.uid
+        }
+        
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, {new: true});
+
+        
+
+        res.status(201).json(
+            {
+                ok:true,
+                eventoActualizado
+               
+            })
+    } catch (error) {
+        res.status(500).json({ok: false, msg: 'Hable con el administrador'})
+        
+    }
+
     
 };
 
